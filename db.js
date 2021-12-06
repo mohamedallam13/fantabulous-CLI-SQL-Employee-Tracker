33f6488a87615e61831790a79ee40a3b44d8faef
mohamedallam13 <mohamedallam.tu@gmail.com>
2021-12-06 01:38:27 +0100

    Add classes and functions in Index

--- db.js
+++ db.js
@@ -1,68 +1,71 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const util = require('util');
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const fs = require("fs");
 
-const CREDENTIALS_JSON_PATH = './dbcredentials.json';
+const CREDENTIALS_JSON_PATH = "./dbcredentials.json";
 
-const connect = () => {
-    var savedCredentials = getSavedCredentials();
-    if (!savedCredentials) {
-        return promptUserForUserNamePassword();
-    } else {
-        return connectToSQL(user, password);
-    }
-}
+const connect = async () => {
+  const { user, password } = getSavedCredentials();
+  let db;
+  if (!user || !password) {
+    // console.log(`Connecting to database..`)
+    db = await promptUserForUserNamePassword();
+  } else {
+    db = connectToSQL(user, password);
+  }
+  return db;
+};
 
 const getSavedCredentials = () => {
-    const credentialsString = fs.readFile(CREDENTIALS_JSON_PATH, 'utf8', function (err, data) {
-        if (err) {
-            return console.log(err);
-        }
-        return JSON.parse(credentialsString);
-    })
-}
+  try {
+    const credentialsString = fs.readFileSync(CREDENTIALS_JSON_PATH, "utf8");
+    return JSON.parse(credentialsString);
+  } catch (err) {
+    console.log(`No saved password found!`);
+    return;
+  }
+};
 
-const promptUserForUserNamePassword = () => {
-    inquirer
-        .prompt([
-            {
-                type: "input",
-                name: "user",
-                message: "User:"
-            },
-            {
-                type: "password",
-                name: "password",
-                message: "Password:"
-            }
-        ])
-        .then((responses) => {
-            const { user, password } = responses;
-            if (!user || !password) {
-                console.log("Please Enter the user name and password again.");
-                promptUserForUserNamePassword();
-                return;
-            };
-            fs.writeFile(CREDENTIALS_JSON_PATH, result, 'utf8', function (err) {
-                if (err) return console.log(err);
-            });
-            return connectToSQL(user, password);
-        })
-}
+const promptUserForUserNamePassword = async () => {
+  // console.log("Please enter SQL credentials")
+  const responses = await inquirer.prompt([
+    {
+      type: "input",
+      name: "user",
+      message: "User:",
+    },
+    {
+      type: "password",
+      name: "password",
+      message: "Password:",
+    },
+  ]);
+  const { user, password } = responses;
+  const db = connectToSQL(user, password);
+  fs.writeFileSync(
+    CREDENTIALS_JSON_PATH,
+    JSON.stringify(responses),
+    "utf8",
+    function (err) {
+      if (err) return console.log(err);
+    }
+  );
+  return connectToSQL(user, password);
+};
 
 const connectToSQL = (user, password) => {
-    const db = mysql.createConnection(
-        {
-            host: 'localhost',
-            // MySQL username,
-            user: user, //'root'
-            // MySQL password
-            password: password, // 'rickandmorty13'
-            database: 'employees_db'
-        },
-        console.log(`Connected to the employees_db database.`)
-    );
-    return db;
-}
+  const db = mysql.createConnection({
+    host: "localhost",
+    // MySQL username,
+    user: user, //'root'
+    // MySQL password
+    password: password, // 'rickandmorty13'
+    database: "employees_db",
+  });
+  console.log(`Connected to the employees_db database.`);
+  return db;
+};
 
-module.exports = util.promisify(connect);
+// connect();
+
+module.exports = connect;
--- index.js
+++ index.js
@@ -1,62 +1,78 @@
-const inquirer = require('inquirer');
-const mysql = require('mysql2');
-const cTable = require('console.table');
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const cTable = require("console.table");
 
-const connect = require('./db.js');
+const connect = require("./db");
 var db;
 
-const ALL_METHODS = {
+const ALL_CLASSES = {
+  Employee: require("./classes/Employee"),
+  Role: require("./classes/Role"),
+  Department: require("./classes/Department")
+};
 
-}
-
-
-const MAIN_MENU = [
-    // 'View Employees',
-    // // 'Add Employee',
-    // // 'Update Employee Role',
-    // 'View All Roles',
-    // 'Add Role',
-    'View All Departments',
-    // 'Add Department',
-    'Quit'
-]
-
+const MAIN_MENU = {
+  'View Employees': { className: 'Employee', method: 'getInfo' },
+  'Add Employee': { className: 'Employee', method: 'add' },
+  'Update Employee Role' : { className: 'Employee', method: 'update' },
+  'View All Roles' : { className: 'Role', method: 'getInfo' },
+  'Add Role': { className: 'Role', method: 'add' },
+  'View All Departments': { className: 'Department', method: 'getInfo' },
+  'Add Department':{className:'Department', method: 'add'},
+  'Quit': {}
+};
 
 const QUESTIONS = [
-    {
-        type: "list",
-        name: "request",
-        message: "What would you like to do?",
-        choices: MAIN_MENU
-    },
-    {
-        type: "confirm",
-        name: "confirm_quit",
-        message: "Are you sure you want to quit?",
-        when: (answers) => answers.request == "Quit"
-    }
-]
+  {
+    type: "list",
+    name: "request",
+    message: "What would you like to do?",
+    choices: Object.keys(MAIN_MENU)
+    ,
+  },
+  {
+    type: "confirm",
+    name: "confirm_quit",
+    message: "Are you sure you want to quit?",
+    when: (answers) => answers.request == "Quit",
+  },
+];
 
-
-//Recursive
 const main = () => {
-    connect().then(dbObj => {
-        db = dbObj;
-        runMainMenu();
-    });
+  // console.log(message);
+  connect().then((conn) => {
+    db = conn;
+    runMainMenu();
+  });
+};
+
+const queryCallBack = (err, data) => {
+  if (err) {
+    console.log(err)
+    return;
+  }
+  return data;
 }
 
 const runMainMenu = () => {
-    inquirer
-        .prompt(QUESTIONS)
-        .then((responses) => {
-            const { request, confirm_quit } = responses
-            if (confirm_quit) {
-                console.log("Good Bye!")
-                db.end();
-                return;
-            }
-        });
+  inquirer.prompt(QUESTIONS).then((responses) => {
+    const { request, confirm_quit } = responses;
+    if (confirm_quit) {
+      console.log("Good Bye!");
+      db.end();
+      return;
+    } else {
+      fulfillRequest(request);
+      runMainMenu();
+    }
+  });
+};
+
+function fulfillRequest(request) {
+  const { className, method } = MAIN_MENU[request];
+  const targetClass = new ALL_CLASSES[className](db)
+  targetClass[method](queryCallBack);
 }
+
 
 main();
